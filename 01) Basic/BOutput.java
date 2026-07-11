c6d543439fc53b487b8e0a233a9bbfc7accd590a
Stuti- Sharma21 <estellarselwyn@gmail.com>
2026-07-11 12:04:52 +0530

    learned print and println statment

---
+++ 01) Basic/BOutput.java
@@ -0,0 +1,25 @@
+public class BOutput{
+    public static void main(String[] args){
+        // to print the output in java
+
+        // println -> to print in next line
+        System.out.println("i am printing my first java program");
+        System.out.println("java is fun");
+        System.out.println("its interesting so far");
+
+        // print -> it doesn't insert a new line at the end of output
+        System.out.print("hello world");
+        System.out.print(" it is being printed in the same line");
+         
+        // for numbers inside println we dont usse ""
+        System.out.println(20);
+        System.out.println(21);
+        System.out.println(22);
+
+        //operations can be performed inside println for numbers
+        System.out.println(20 + 21);
+        System.out.println(20 - 21);
+        System.out.println(20 * 21);
+        System.out.println(20 / 21);
+    }
+}
