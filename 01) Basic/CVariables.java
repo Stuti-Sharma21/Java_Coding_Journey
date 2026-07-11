dffcfda6ee11b551fb19601702d8bca47a4b42a6
Stuti- Sharma21 <estellarselwyn@gmail.com>
2026-07-11 13:23:16 +0530

    final keyword

--- 01) Basic/CVariables.java
+++ 01) Basic/CVariables.java
@@ -17,5 +17,20 @@ public class CVariables{
          boolean LikeJava = true;
          System.out.println(LikeJava);
 
+         // final -> its a keyword which is used to fix the value overwriting the value wont work with final
+
+         int x=40;
+         System.out.println(x);
+         //value is 40 
+         x=50;
+         System.out.println(x);
+         //value will be 50 ie overwritten
+
+         final int y=60;
+         System.out.println(y);
+         
+         // y = 10000; // Error: cannot assign a value to final variable y
+
+
     }
 }
