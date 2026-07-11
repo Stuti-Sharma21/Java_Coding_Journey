880e53eb9ff46847fd97d0a485150ed1f62977f3
Stuti- Sharma21 <estellarselwyn@gmail.com>
2026-07-11 13:13:14 +0530

    understanding variables

---
+++ 01) Basic/CVariables.java
@@ -0,0 +1,21 @@
+public class CVariables{
+    public static void main(String[] args){
+         // variables are containers used to store data
+
+         String name = "Sam";
+         System.out.println(name);
+
+         int age = 20;
+         System.out.println(age);
+
+         float cgpa = 98.9f;
+         System.out.println(cgpa);
+
+         char grade= 'A';
+         System.out.println(grade);
+
+         boolean LikeJava = true;
+         System.out.println(LikeJava);
+
+    }
+}
