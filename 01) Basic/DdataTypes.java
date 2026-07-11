604ba8099393df6762d0ee164316a7f581a4435d
Stuti- Sharma21 <estellarselwyn@gmail.com>
2026-07-11 14:20:47 +0530

    Data types

---
+++ 01) Basic/DdataTypes.java
@@ -0,0 +1,29 @@
+public class DdataTypes{
+    public static void main(String[] args){
+
+         // int -> Stores whole numbers from -2,147,483,648 to 2,147,483,647
+         int x=8392755;
+
+         // long -> Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+         long y=839028908409275L;
+
+         // float -> Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+         float a=78.54334f;
+
+         // double -> Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+         double b=578.38757198759;
+
+         // boolean -> 	Stores true or false values
+         boolean isTrue=true;
+
+         // char -> Stores a single character/letter or ASCII values
+         char ch='a';
+
+         // short -> Stores whole numbers from -32,768 to 32,767
+         short s=5426;
+
+         //Primitive types in Java are predefined and built into the language, while non-primitive types are created by the programmer (except for String).
+         //Primitive have to hold value while non primitive can be NULL 
+
+    }
+}
